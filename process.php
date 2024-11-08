eff2f7be2147b8e83656d090272b9fb7c0f7f7c4
Paul Vincent Magaling <paulvincentaniciete1012@gmail.com>
2024-11-08 22:01:35 +0800

    Nothing

--- process.php
+++ process.php
@@ -8,28 +8,28 @@
 
 extract($_POST); //Extracting the data being passed from the JSON
 
-// function expand_url($url)
-// {
-//     //Get response headers
-//     $response = get_headers($url, 1);
-//     //Get the location property of the response header. If failure, return original url
-//     if (array_key_exists('Location', $response)) {
-//         $location = $response["Location"];
-//         if (is_array($location)) {
-//             // t.co gives Location as an array
-//             return expand_url($location[count($location) - 1]);
-//         } else {
-//             return expand_url($location);
-//         }
-//     }
-//     return $url;
-// }
+/* function expand_url($url)
+{
+    //Get response headers
+    $response = get_headers($url, 1);
+    //Get the location property of the response header. If failure, return original url
+    if (array_key_exists('Location', $response)) {
+        $location = $response["Location"];
+        if (is_array($location)) {
+            // t.co gives Location as an array
+            return expand_url($location[count($location) - 1]);
+        } else {
+            return expand_url($location);
+        }
+    }
+    return $url;
+}
 
-// // For readability
-// function test_expand_url($short_url)
-// {
-//     return $actual_long_url = expand_url($short_url);
-// }
+// For readability
+function test_expand_url($short_url)
+{
+    return $actual_long_url = expand_url($short_url);
+} */
 
 function expand_url($url)
 {
